737830b9cb54bea65ab16ced0bc31d90a1052ab9
Bryan Ramírez Correa <bryan.ramirez.correa@ibm.com>
2023-07-28 10:13:53 +0200

    streamlit app

--- app.py
+++ app.py
@@ -1,104 +1,45 @@
-import os
-import logging
-from flask import Flask, request, render_template, redirect, url_for, session
+from operator import index
+import streamlit as st
+import plotly.express as px
+from pycaret.regression import setup, compare_models, pull, save_model, load_model
+import pandas_profiling
 import pandas as pd
-import plotly.graph_objects as go
+from streamlit_pandas_profiling import st_profile_report
+import os 
 
-app = Flask(__name__, static_folder='static')
-app.secret_key = 'your_secret_key'
+if os.path.exists('./dataset.csv'): 
+    df = pd.read_csv('dataset.csv', index_col=None)
 
-# Ruta de inicio, redirecciona a la página principal
-@app.route('/')
-def index():
-    return redirect(url_for('main'))
+with st.sidebar: 
+    st.image("https://www.onepointltd.com/wp-content/uploads/2020/03/inno2.png")
+    st.title("Automodeladorv.1")
+    choice = st.radio("Navigation", ["subir fichero","Análisis descriptivo","Modelaje", "Descargar modelo"])
+    st.info("Esta aplicación automatiza el proceso de creación de un modelo.")
 
-# Ruta para la página principal (Inicio)
-@app.route('/main')
-def main():
-    return render_template('index.html')
+if choice == "subir fichero":
+    st.title("subir fichero")
+    file = st.file_uploader("subir fichero")
+    if file: 
+        df = pd.read_csv(file, index_col=None)
+        df.to_csv('dataset.csv', index=None)
+        st.dataframe(df)
 
-# Ruta para la página de carga de archivo
-@app.route('/upload', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        file = request.files['file']
-        if file:
-            logging.info("Nombre del archivo enviado: %s", file.filename)
-            filename = file.filename
-            file_path = os.path.join('uploads', filename)
-            file.save(file_path)
-            logging.info("Archivo guardado en: %s", file_path)
+if choice == "Análisis descriptivo":
+    st.title("Exploratory Data Analysis")
+    profile_df = df.profile_report()
+    st_profile_report(profile_df)
 
-            # Almacenar el nombre del archivo en la sesión
-            session['filename'] = filename
+if choice == "Modelaje":
+    chosen_target = st.selectbox('Choose the Target Column', df.columns)
+    if st.button('Run Modelling'): 
+        setup(df, target=chosen_target, silent=True)
+        setup_df = pull()
+        st.dataframe(setup_df)
+        best_model = compare_models()
+        compare_df = pull()
+        st.dataframe(compare_df)
+        save_model(best_model, 'best_model')
 
-            return redirect(url_for('process_file'))
-    return render_template('upload.html')
-
-# Ruta para procesar el archivo y mostrar las estadísticas
-@app.route('/process')
-def process_file():
-    try:
-        filename = session.get('filename')
-
-        if filename is None:
-            return redirect(url_for('upload_file'))
-
-        file_path = os.path.join('uploads', filename)
-        logging.info("Procesando archivo: %s", file_path)
-
-        # Leer el archivo CSV o Excel utilizando pandas
-        if filename.endswith('.csv'):
-            df = pd.read_csv(file_path)
-        elif filename.endswith('.xls') or filename.endswith('.xlsx'):
-            df = pd.read_excel(file_path, engine='xlrd')
-        else:
-            return "Formato de archivo no compatible. Debe ser un archivo CSV o Excel."
-
-        # Obtener estadísticas adicionales
-        null_counts = df.isnull().sum().to_list()
-        missing_counts = df.isna().sum().to_list()
-        info = df.info().replace('\n', '<br>') if df.info() else None
-
-        # Realizar el "shape" y "describe" del DataFrame
-        shape_info = df.shape
-        logging.info("Shape del DataFrame: %s", shape_info)
-        describe_info = df.describe().to_html()
-        logging.info("Describe del DataFrame:\n%s", describe_info)
-
-        return render_template('stats.html', filename=filename, shape_info=shape_info, describe_info=describe_info, null_counts=null_counts, missing_counts=missing_counts, info=info)
-
-    except Exception as e:
-        return render_template('error.html', error_message=str(e))
-
-# Ruta para la página del gráfico
-@app.route('/graph')
-def show_graph():
-    try:
-        filename = session.get('filename')
-
-        if filename is None:
-            return redirect(url_for('upload_file'))
-
-        file_path = os.path.join('uploads', filename)
-        logging.info("Procesando archivo: %s", file_path)
-
-        # Leer el archivo CSV o Excel utilizando pandas
-        if filename.endswith('.csv'):
-            df = pd.read_csv(file_path)
-        elif filename.endswith('.xls') or filename.endswith('.xlsx'):
-            df = pd.read_excel(file_path, engine='xlrd')
-        else:
-            return "Formato de archivo no compatible. Debe ser un archivo CSV o Excel."
-
-        # Crear gráfico utilizando Plotly
-        fig = go.Figure(data=[go.Bar(x=df.columns, y=df.count())])
-        graph = fig.to_html(full_html=False)
-
-        return render_template('graph.html', graph=graph)
-
-    except Exception as e:
-        return render_template('error.html', error_message=str(e))
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if choice == "Descargar modelo":
+    with open('best_model.pkl', 'rb') as f: 
+        st.download_button('Download Model', f, file_name="best_model.pkl")
