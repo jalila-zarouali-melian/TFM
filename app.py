c70458cdfe096ef3eb4168b9bb4e3ed1ab17c1a1
Bryan Ramírez Correa <bryan.ramirez.correa@ibm.com>
2023-07-30 18:40:10 +0200

    streamlit app, con error de libreria resuelto

--- app.py
+++ app.py
@@ -7,10 +7,17 @@ import pandas as pd
 from streamlit_pandas_profiling import st_profile_report
 import os 
 
-if os.path.exists('./dataset.csv'): 
-    df = pd.read_csv('dataset.csv', index_col=None)
+# Helper function to load the DataFrame using caching
+@st.cache(allow_output_mutation=True)
+def load_dataframe():
+    if os.path.exists('./dataset.csv'):
+        return pd.read_csv('dataset.csv', index_col=None)
+    return None
 
-with st.sidebar: 
+# Load the DataFrame
+df = load_dataframe()
+
+with st.sidebar:
     st.image("https://www.onepointltd.com/wp-content/uploads/2020/03/inno2.png")
     st.title("Automodeladorv.1")
     choice = st.radio("Navigation", ["subir fichero","Análisis descriptivo","Modelaje", "Descargar modelo"])
@@ -19,27 +26,37 @@ with st.sidebar:
 if choice == "subir fichero":
     st.title("subir fichero")
     file = st.file_uploader("subir fichero")
-    if file: 
+    if file:
         df = pd.read_csv(file, index_col=None)
         df.to_csv('dataset.csv', index=None)
         st.dataframe(df)
 
 if choice == "Análisis descriptivo":
     st.title("Exploratory Data Analysis")
-    profile_df = df.profile_report()
-    st_profile_report(profile_df)
+    if df is not None:
+        profile_df = df.profile_report()
+        st_profile_report(profile_df)
+    else:
+        st.warning("Please upload a dataset first.")
 
 if choice == "Modelaje":
-    chosen_target = st.selectbox('Choose the Target Column', df.columns)
-    if st.button('Run Modelling'): 
-        setup(df, target=chosen_target, silent=True)
-        setup_df = pull()
-        st.dataframe(setup_df)
-        best_model = compare_models()
-        compare_df = pull()
-        st.dataframe(compare_df)
-        save_model(best_model, 'best_model')
+    if df is not None:
+        chosen_target = st.selectbox('Choose the Target Column', df.columns)
+        if st.button('Run Modelling'):
+            # Convert 'price' column to numeric, coercing non-numeric values to NaN
+            setup(df, target=chosen_target, categorical_features=['Sex'])  # Specify 'Sex' as a categorical feature
+            setup_df = pull()
+            st.dataframe(setup_df)
+            best_model = compare_models()
+            compare_df = pull()
+            st.dataframe(compare_df)
+            save_model(best_model, 'best_model')
+    else:
+        st.warning("Please upload a dataset first.")
 
 if choice == "Descargar modelo":
-    with open('best_model.pkl', 'rb') as f: 
-        st.download_button('Download Model', f, file_name="best_model.pkl")
+    if df is not None:
+        with open('best_model.pkl', 'rb') as f:
+            st.download_button('Download Model', f, file_name="best_model.pkl")
+    else:
+        st.warning("Please upload a dataset first.")
